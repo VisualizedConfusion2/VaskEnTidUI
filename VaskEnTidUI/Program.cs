f9982a0576e0b099fd9efe2ea51862dc31740f4a
mal025 <mal025@edu.zealand.dk>
2025-10-20 12:51:29 +0200

    Created table for showing Error Reports

--- VaskEnTidUI/Program.cs
+++ VaskEnTidUI/Program.cs
@@ -1,5 +1,6 @@
 using VaskEnTidLib.Services;
 using VaskEnTidLib.Repositories;
+using Microsoft.IdentityModel.Tokens;
 
 namespace VaskEnTidUI
 {
@@ -8,18 +9,24 @@ namespace VaskEnTidUI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            string _connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LaundryManagementDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False;Application Name=\"SQL Server Management Studio\";Command Timeout=30";
 
             // Add services to the container.
             builder.Services.AddRazorPages();
 
-            builder.Services.AddSingleton(new MachineRepo("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LaundryManagementDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False;Application Name=\"SQL Server Management Studio\";Command Timeout=30"));
+            builder.Services.AddSingleton(new MachineRepo(_connectionString));
             builder.Services.AddSingleton<MachineService>();
 
-            builder.Services.AddSingleton(new UserRepo("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LaundryManagementDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False;Application Name=\"SQL Server Management Studio\";Command Timeout=30"));
+            builder.Services.AddSingleton(new UserRepo(_connectionString));
             builder.Services.AddSingleton<UserService>();
 
-            builder.Services.AddSingleton(new BookingRepo("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LaundryManagementDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False;Application Name=\"SQL Server Management Studio\";Command Timeout=30"));
+            builder.Services.AddSingleton(new BookingRepo(_connectionString));
             builder.Services.AddSingleton<BookingService>();
+
+            builder.Services.AddSingleton(new ErrorReportRepo(_connectionString));
+            builder.Services.AddSingleton<ErrorReportService>();
+
+
 
             builder.Services.AddDistributedMemoryCache(); // Stores session in memory
             builder.Services.AddSession(options =>
